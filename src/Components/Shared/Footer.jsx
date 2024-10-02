3c27a212c216a49e6d2cb9215d2a5af0bda6ee00
Syed-Farazuddin <syedfarazuddin73@gmail.com>
2024-10-02 11:50:47 +0530

    warning removed

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -9,26 +9,41 @@ const Footer = () => {
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
         <div className="mt-16 basis-1/2 md:mt-0">
           <img className="w-20 h-20" alt="logo" src={Logo} />
-          <p className="my-5">
+          <div className="my-5">
             <h1 className="font-semibold">How to Reach Us</h1>
             <p>471 5th Ave.</p>
             <p>Brooklyn, NY 11215</p>
-          </p>
+          </div>
         </div>
         <div className="w-full md:w-1/3 mb-3 md:mb-0">
-            <h4 className="text-lg font-bold mb-4">Socials</h4>
-            <div className="flex space-x-6">
-              <a href="https://www.facebook.com/sipnplaynyc/" target="_blank" rel="noopener noreferrer" className="hover:text-blue-600 transition-colors">
-                <FaFacebook className="w-8 h-8 hover:animate-bounce" />
-              </a>
-              <a href="https://www.instagram.com/sipnplaynyc/?hl=en" target="_blank" rel="noopener noreferrer" className="hover:text-pink-600 transition-colors">
-                <FaInstagram className="w-8 h-8 hover:animate-bounce" />
-              </a>
-              <a href="https://www.tiktok.com/@sipnplaynycofficial?lang=en" target="_blank" rel="noopener noreferrer" className="hover:text-violet-500 transition-colors">
-                <FaTiktok className="w-8 h-8 hover:animate-bounce" />
-              </a>
-            </div>
+          <h4 className="text-lg font-bold mb-4">Socials</h4>
+          <div className="flex space-x-6">
+            <a
+              href="https://www.facebook.com/sipnplaynyc/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-blue-600 transition-colors"
+            >
+              <FaFacebook className="w-8 h-8 hover:animate-bounce" />
+            </a>
+            <a
+              href="https://www.instagram.com/sipnplaynyc/?hl=en"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-pink-600 transition-colors"
+            >
+              <FaInstagram className="w-8 h-8 hover:animate-bounce" />
+            </a>
+            <a
+              href="https://www.tiktok.com/@sipnplaynycofficial?lang=en"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-violet-500 transition-colors"
+            >
+              <FaTiktok className="w-8 h-8 hover:animate-bounce" />
+            </a>
           </div>
+        </div>
 
         <div className="w-full md:w-1/3">
           <h4 className="text-lg font-bold mb-4">Contact Us</h4>
