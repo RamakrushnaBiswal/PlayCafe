76ad8106ea86e9493ff582f0b06262ef4d787ddd
Sajal Batra <sajalbatra7@gmail.com>
2024-10-03 21:01:53 +0530

    fixed the about section

--- frontend/src/components/Pages/About.jsx
+++ frontend/src/components/Pages/About.jsx
@@ -7,37 +7,42 @@ export default function About() {
         className="absolute inset-0 bg-center bg-cover"
         style={{ backgroundImage: `url(${bgpic})` }}
       >
-        {/* Black overlay with 60% opacity */}
+        {/* Black overlay with 40% opacity */}
         <div className="absolute inset-0 bg-black opacity-60"></div>
 
         {/* Content container */}
-        <div className="relative z-10 flex flex-col justify-center h-full px-4 text-center md:flex-row md:px-10 lg:px-20 md:text-left">
-          {/* Title */}
-          <div className="flex items-center justify-center md:w-1/2">
-            <h1 className="text-5xl font-bold text-center text-white sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl">
-              ABOUT US
-            </h1>
-          </div>
+        <div className="relative z-10 grid items-center justify-center h-full grid-cols-1 p-4 md:grid-cols-2">
+          {/* Heading */}
+          <h1 className="p-4 text-4xl font-bold text-center text-white md:text-7xl lg:text-9xl">
+            ABOUT US
+          </h1>
 
           {/* Paragraph */}
-          <div className="relative z-10 flex justify-center w-full mt-10 align-middle md:pl-32 md:mt-32">
-            <p className="text-gray-200 text-l md:text-xl"> {/* Change text color for better contrast */}
+          <div className="relative z-10 flex justify-center w-full mt-6 md:mt-0 md:px-36 ">
+            <p className="w-full text-sm text-gray-200 md:text-base lg:text-xl">
               How it works..
               Our name says it all!
               Founder, Jonathan Li, shares a passion for board games, boba, and
               delicious food, so he combined them all to become Sip & Play, Park Slope’s
               first board game cafe. It is a straightforward concept, come in with your
-              friends and family to play any board game from our library of <span className="text-amber-600">300+ games!</span>
+              friends and family to play any board game from our library of{" "}
+              <span className="text-amber-600">300+ games!</span>
               We hope when you visit, you also enjoy our coffee, espresso, boba,
               sandwiches, and snacks!
               <br />
               <br />
-              Hours and Location
-              New opening hours:<br />
-              Sunday: 10am-11pm <br />
-              Mon-Thurs: 11am-11pm <br />
-              Fri: 11am-midnight <br />
-              Sat: 10am-midnight <br />
+              <strong>Hours and Location</strong>
+              <br />
+              New opening hours:
+              <br />
+              Sunday: 10am-11pm
+              <br />
+              Mon-Thurs: 11am-11pm
+              <br />
+              Fri: 11am-midnight
+              <br />
+              Sat: 10am-midnight
+              <br />
               Our kitchen closes 2.5-3 hours before we close!
             </p>
           </div>
