c3336efec6c101541df4cd030ff879833e9b46e1
Arindam <17arindambera@gmail.com>
2024-10-04 14:50:16 +0530

    kinde auth integrated

--- frontend/src/components/Shared/Navbar.jsx
+++ frontend/src/components/Shared/Navbar.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import Logo from "../../assets/Logo/logo.png";
 import { Link, useLocation } from "react-router-dom";
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 
 const Navbar = () => {
+  const { login, register, logout, isAuthenticated } = useKindeAuth();  // Added logout and isAuthenticated
   const [isScrolled, setIsScrolled] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const location = useLocation();
+
 
   useEffect(() => {
     const handleScroll = () => {
@@ -27,17 +30,19 @@ const Navbar = () => {
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
   const isHomePage = location.pathname === "/";
   const buttonTextClass = isScrolled
     ? "text-gray-900"
     : isHomePage
-      ? "text-white"
-      : "text-black";
+    ? "text-white"
+    : "text-black";
 
   return (
     <nav
-      className={`w-full fixed top-0 z-50 transition duration-300 ${isScrolled ? "bg-[#E0F0B1]" : "bg-transparent"
-        } 
+      className={`w-full fixed top-0 z-50 transition duration-300 ${
+        isScrolled ? "bg-[#E0F0B1]" : "bg-transparent"
+      } 
                    ${isScrolled ? "text-gray-800" : "text-black"} 
                    ${isScrolled ? "shadow-lg" : ""}`}
     >
@@ -56,36 +61,40 @@ const Navbar = () => {
             <ul className="ml-4 flex space-x-4 font-semibold">
               <li>
                 <Link
-                  to={"/"}
-                  className={`hover:${isScrolled ? "text-gray-900" : "text-gray-800"
-                    }`}
+                  to="/"
+                  className={`hover:${
+                    isScrolled ? "text-gray-900" : "text-gray-800"
+                  }`}
                 >
                   Home
                 </Link>
               </li>
               <li>
                 <Link
-                  to={"/events"}
-                  className={`hover:${isScrolled ? "text-gray-900" : "text-gray-800"
-                    }`}
+                  to="/events"
+                  className={`hover:${
+                    isScrolled ? "text-gray-900" : "text-gray-800"
+                  }`}
                 >
                   Events
                 </Link>
               </li>
               <li>
                 <Link
-                  to={"/menu"}
-                  className={`hover:${isScrolled ? "text-gray-900" : "text-gray-800"
-                    }`}
+                  to="/menu"
+                  className={`hover:${
+                    isScrolled ? "text-gray-900" : "text-gray-800"
+                  }`}
                 >
                   Menu
                 </Link>
               </li>
               <li>
                 <Link
-                  to={"/reservation"}
-                  className={`hover:${isScrolled ? "text-gray-900" : "text-gray-800"
-                    }`}
+                  to="/reservation"
+                  className={`hover:${
+                    isScrolled ? "text-gray-900" : "text-gray-800"
+                  }`}
                 >
                   Reservation
                 </Link>
@@ -93,14 +102,52 @@ const Navbar = () => {
               <li>
                 <Link
                   to="/boardgame"
-                  className={`hover:${isScrolled ? "text-gray-900" : "text-gray-800"
-                    }`}
+                  className={`hover:${
+                    isScrolled ? "text-gray-900" : "text-gray-800"
+                  }`}
                 >
                   Boardgames
                 </Link>
               </li>
+
+              {/* If the user is authenticated, show the Logout button */}
+              {isAuthenticated ? (
+                <button
+                  onClick={logout}
+                  className={`hover:${
+                    isScrolled ? "text-gray-900" : "text-gray-800"
+                  }`}
+                  type="button"
+                >
+                  Log Out
+                </button>
+              ) : (
+                <>
+                  {/* If the user is not authenticated, show Login and Register */}
+                  <button
+                    onClick={register}
+                    className={`hover:${
+                      isScrolled ? "text-gray-900" : "text-gray-800"
+                    }`}
+                    type="button"
+                  >
+                    Register
+                  </button>
+                  <button
+                    onClick={login}
+                    className={`hover:${
+                      isScrolled ? "text-gray-900" : "text-gray-800"
+                    }`}
+                    type="button"
+                  >
+                    Log In
+                  </button>
+                </>
+              )}
             </ul>
           </div>
+
+          {/* Mobile Menu Button */}
           <div className="flex md:hidden">
             <button
               onClick={toggleMenu}
@@ -124,48 +171,86 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+
       {/* Mobile Menu */}
       {isMenuOpen && (
         <div
-          className={`md:hidden ${isScrolled ? "bg-amber-100 shadow-lg" : "bg-[#E0F0B1] shadow-lg"
-            }`}
+          className={`md:hidden ${
+            isScrolled ? "bg-amber-100 shadow-lg" : "bg-[#E0F0B1] shadow-lg"
+          }`}
         >
           <div className="px-4 pt-4 pb-4 space-y-2">
             <a
               href="/"
-              className={`block px-4 py-3 rounded-md text-base font-semibold transition duration-300 ${isScrolled ? "text-gray-900" : "text-gray-800"
-                } hover:bg-amber-300 hover:text-black`}
+              className={`block px-4 py-3 rounded-md text-base font-semibold transition duration-300 ${
+                isScrolled ? "text-gray-900" : "text-gray-800"
+              } hover:bg-amber-300 hover:text-black`}
             >
               Home
             </a>
             <a
-              href="/event"
-              className={`block px-4 py-3 rounded-md text-base font-semibold transition duration-300 ${isScrolled ? "text-gray-900" : "text-gray-800"
-                } hover:bg-amber-300 hover:text-black`}
+              href="/events"
+              className={`block px-4 py-3 rounded-md text-base font-semibold transition duration-300 ${
+                isScrolled ? "text-gray-900" : "text-gray-800"
+              } hover:bg-amber-300 hover:text-black`}
             >
               Events
             </a>
             <a
               href="/menu"
-              className={`block px-4 py-3 rounded-md text-base font-semibold transition duration-300 ${isScrolled ? "text-gray-900" : "text-gray-800"
-                } hover:bg-amber-300 hover:text-black`}
+              className={`block px-4 py-3 rounded-md text-base font-semibold transition duration-300 ${
+                isScrolled ? "text-gray-900" : "text-gray-800"
+              } hover:bg-amber-300 hover:text-black`}
             >
               Menu
             </a>
             <a
-              href="/register"
-              className={`block px-4 py-3 rounded-md text-base font-semibold transition duration-300 ${isScrolled ? "text-gray-900" : "text-gray-800"
-                } hover:bg-amber-300 hover:text-black`}
+              href="/reservation"
+              className={`block px-4 py-3 rounded-md text-base font-semibold transition duration-300 ${
+                isScrolled ? "text-gray-900" : "text-gray-800"
+              } hover:bg-amber-300 hover:text-black`}
             >
               Reservation
             </a>
             <a
               href="/boardgame"
-              className={`block px-4 py-3 rounded-md text-base font-semibold transition duration-300 ${isScrolled ? "text-gray-900" : "text-gray-800"
-                } hover:bg-amber-300 hover:text-black`}
+              className={`block px-4 py-3 rounded-md text-base font-semibold transition duration-300 ${
+                isScrolled ? "text-gray-900" : "text-gray-800"
+              } hover:bg-amber-300 hover:text-black`}
             >
               Boardgames
             </a>
+
+            {/* Mobile Menu - Show Logout if authenticated, else Login/Register */}
+            {isAuthenticated ? (
+              <button
+                onClick={logout}
+                className={`block w-full text-left px-4 py-3 rounded-md text-base font-semibold transition duration-300 ${
+                  isScrolled ? "text-gray-900" : "text-gray-800"
+                } hover:bg-amber-300 hover:text-black`}
+              >
+                Log Out
+              </button>
+            ) : (
+              <>
+                <button
+                  onClick={register}
+                  className={`block w-full text-left px-4 py-3 rounded-md text-base font-semibold transition duration-300 ${
+                    isScrolled ? "text-gray-900" : "text-gray-800"
+                  } hover:bg-amber-300 hover:text-black`}
+                >
+                  Register
+                </button>
+                <button
+                  onClick={login}
+                  className={`block w-full text-left px-4 py-3 rounded-md text-base font-semibold transition duration-300 ${
+                    isScrolled ? "text-gray-900" : "text-gray-800"
+                  } hover:bg-amber-300 hover:text-black`}
+                >
+                  Log In
+                </button>
+              </>
+            )}
           </div>
         </div>
       )}
