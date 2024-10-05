206280cb64023545220931799aedd3d57235d26f
Samarth Vaidya <v.samarth1212@gmail.com>
2024-10-05 14:07:37 +0530

    Complete Backend Error system created with Winston Logger library

--- backend/routes/feedbackRouter.js
+++ backend/routes/feedbackRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { createFeedback } = require("../controller/feedback.controller");
 const router = express.Router();
 const apiInfo = require("../config/api.info");
+const logger = require("../config/logger"); // Import your logger
 
 router.post("/create", createFeedback);
 
@@ -9,6 +10,10 @@ router.get("/", (req, res) => {
   try {
     res.json(apiInfo);
   } catch (error) {
+    logger.error("Error fetching API info:", {
+      message: error.message,
+      stack: error.stack,
+    });
     res.status(500).json({ error: "Internal server error" });
   }
 });
