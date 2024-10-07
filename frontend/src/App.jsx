33972d7a18d39784503ffdbe106c9629266e49bc
maitri-vv <maitrivaghasiya99@gmail.com>
2024-10-07 09:32:59 +0530

    Updated the CSS for button
    I have resolved the error and updated the CSS for button
    Closes Issue: #84

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 // src/App.js
-import React from 'react';
 import './App.css';
 import Navbar from './components/Shared/Navbar';
 import Footer from './components/Shared/Footer';
 import { Outlet } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [showButton, setShowButton] = useState(false);
@@ -29,6 +29,25 @@ function App() {
       <Navbar />
       <Outlet />
       <Footer />
+
+      {/* Back to Top Button */}
+      {showButton && (
+        <button
+          onClick={scrollToTop}
+          className="fixed bottom-2 right-2 p-4 bg-black text-white rounded-full flex justify-center items-center shadow-lg cursor-pointer animate-bounce"
+        >
+          {/* SVG Arrow */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-8 h-8"  // Adjust size as needed
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 15l7-7 7 7" />
+          </svg>
+        </button>
+      )}
     </div>
   );
 }
