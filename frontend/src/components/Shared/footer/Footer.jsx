ecd28f51bc6171b716be574c74ac867f4fe1d32a
Sawan <sawankushwah457@gmail.com>
2024-10-08 19:45:00 +0530

    fixes footer scroll effect

--- frontend/src/components/Shared/footer/Footer.jsx
+++ frontend/src/components/Shared/footer/Footer.jsx
@@ -1,13 +1,9 @@
-import React from "react";
 import Content from "./Content";
 
 const Footer = () => {
   return (
-    <footer
-      className="relative h-[800px]"
-      style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
-    >
-      <div className="relative h-[calc(100vh+800px)] -top-[100vh]">
+    <footer >
+      <div className="bottom-0">
         <div className="h-[600px] sticky top-[calc(100vh-600px)]">
           <Content />
         </div>
