13d5c6d2d55081efddfb4729971e2392008aa43a
Shirisha <tyarlashirisha@gmail.com>
2024-10-12 00:31:49 +0530

    added splide with some more images

--- frontend/src/components/Pages/Boardgame.jsx
+++ frontend/src/components/Pages/Boardgame.jsx
@@ -249,7 +249,7 @@ export default function Boardgame() {
               </SplideSlide>
               <SplideSlide>
                 <LazyLoadImage
-                  alt="bg1"
+                  alt="bg2"
                   effect="blur"
                   src="https://images.unsplash.com/photo-1609818698346-8cb3be6e0bc0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   className="w-full object-cover"
@@ -257,7 +257,7 @@ export default function Boardgame() {
               </SplideSlide>
               <SplideSlide>
                 <LazyLoadImage
-                  alt={'board2'}
+                  alt="bg3"
                   effect="blur"
                   src="https://images.unsplash.com/photo-1659480142923-0cd01191e0e9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                   className="w-full h-full object-cover"
