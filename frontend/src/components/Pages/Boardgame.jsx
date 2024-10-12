62a57b1d23267fbdcb98fe8b116af274e7262b2b
Ramakrushna Biswal <125277258+RamakrushnaBiswal@users.noreply.github.com>
2024-10-12 08:05:48 +0530

    Update Boardgame.jsx

--- frontend/src/components/Pages/Boardgame.jsx
+++ frontend/src/components/Pages/Boardgame.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect  } from 'react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
 
