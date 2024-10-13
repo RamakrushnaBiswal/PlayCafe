292d499ed3115a9486a6be2d24aaadb543ef8e11
Ramakrushna Biswal <125277258+RamakrushnaBiswal@users.noreply.github.com>
2024-10-13 07:56:43 +0530

    Update Signup.jsx

--- frontend/src/components/Pages/Signup.jsx
+++ frontend/src/components/Pages/Signup.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React,{ useState, useEffect } from "react";
 import photo from "../../assets/login.png";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
