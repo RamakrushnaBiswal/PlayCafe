6c3171d6fe9964a050b06d9041d81b60af4054e1
Samarth Vaidya <v.samarth1212@gmail.com>
2024-10-15 19:58:36 +0530

    coderabbit changes 2.0

--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -3,18 +3,24 @@ const logger = require("../config/logger");
 
 const authenticateAdmin = (req, res, next) => {
   const token = req.header("Authorization")?.split(" ")[1]; // Expecting "Bearer <token>"
-
   if (token) {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decoded.role);
-    if (decoded.role !== "admin") {
-      logger.error(
-        `Unauthorized access to admin route: ${JSON.stringify(decoded.id)}`
-      );
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      console.log(decoded.role);
+      if (decoded.role !== "admin") {
+        logger.error(
+          `Unauthorized access to admin route: ${JSON.stringify(decoded.id)}`
+        );
+        return res.status(401).json({ error: "Unauthorized access" });
+      }
+      logger.info(`Admin authenticated: ${JSON.stringify(decoded.sub)}`);
+      next();
+    } catch (error) {
+      logger.error(`Error authenticating admin: ${error}`);
       return res.status(401).json({ error: "Unauthorized access" });
     }
-    logger.info(`Admin authenticated: ${JSON.stringify(decoded.sub)}`);
-    next();
+  } else {
+    return res.status(401).json({ error: "No token was provided" });
   }
 };
 
